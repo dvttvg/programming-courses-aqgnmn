8e18668598616d0a3c693222b039e840116878d6
dvttvg <yasserbenattouch@gmail.com>
2022-02-04 17:02:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-aqgnmn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-aqgnmn)
---
+++ index.ts
@@ -0,0 +1,9 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//###################################################################
+let calculatrice: number;
+let calculatric: number;
+calculatrice = 1;
+calculatric = 2;
+console.log(calculatric + calculatrice);
